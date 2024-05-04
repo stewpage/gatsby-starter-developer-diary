282b132a35bfa19f3b4460e4986a87fc30123396
Pooja <1985.pooja@gmail.com>
2024-05-04 13:48:48 +0200

    try fixing pagination

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,8 +7,7 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>Page Not Found</h1>
-    <p><i>Just lightin&apos; out for the territories,
-huh? Brother, I been there.</i></p>
+    <p><i>Go to Archive to see older posts.</i></p>
   </Layout>
 )
 
